cbae828a0a35cb9e36d98b51f74a09a1a9536bd3
RadaSangOn <radasangon@gmail.com>
2016-06-03 19:24:06 +0700

    rgdsfgdfg

--- FacebookApp/src/FacebookApp/Controllers/HomeController.cs
+++ FacebookApp/src/FacebookApp/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http;
 
 namespace FacebookApp.Controllers
 {
@@ -30,6 +32,35 @@ namespace FacebookApp.Controllers
         public IActionResult Error()
         {
             return View();
+        }
+
+
+        public IActionResult test1()
+        {
+            string res = "";
+            using (WebClient client = new WebClient())
+            {
+                res = client.DownloadString("http://graph.facebook.com/V2.6/rada.sangon?fields=id,name");
+            }
+            Task t = new Task(getFB);
+            t.Start();
+            return Json(new { result = "success", response = res });
+        }
+
+        private async Task<string> getFB()
+        {
+            var responseString = "";
+            using (var client = new HttpClient())
+            {
+                
+                HttpResponseMessage response = await client.GetAsync("http://graph.facebook.com/V2.6/me?fields=id,name");
+                if (response.IsSuccessStatusCode)
+                {
+                    responseString = await response.Content.ReadAsStringAsync();
+                }
+
+            }
+            return responseString;
         }
     }
 }
